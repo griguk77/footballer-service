9c64c62ebe55b48f69d6c2807a7c7eb76ede0c64
griguk77 <griguk77@gmail.com>
2024-03-17 22:03:21 +0300

    fix 9

--- test_footballer_service.py
+++ test_footballer_service.py
@@ -17,13 +17,9 @@ def test_get_all_footballers(url: str):
 
 def test_get_footballer_by_id(url: str):
     res = requests.get(url).json()
-    assert(res == [
-    {'clubs_id': 1, 'name': 'Mbappe',
+    assert(res == {'clubs_id': 1, 'name': 'Mbappe',
      'country': 'France',
-     'goals': '50', 'age': '34'},
-    {'clubs_id': 1, 'name': 'Ronaldo',
-     'country': 'Portugal',
-     'goals': '5', 'age': '26'}])
+     'goals': '50', 'age': '34'})
 
 
 if __name__ == '__main__':
